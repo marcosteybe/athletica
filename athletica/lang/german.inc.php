cc09618b85213049047952333a182d91b1dbb755
rijoll@bluewin.ch <rijoll@bluewin.ch@77e7c866-7b45-0410-9efb-6b44a749e0e8>
2008-04-03 08:42:45 +0000

    Test

--- athletica/lang/german.inc.php
+++ athletica/lang/german.inc.php
@@ -658,6 +658,6 @@ $strBasdataIncluded = "Auch die Stammdaten sichern";
 $strAllMeetings = "[Alle Meetings]";
 $strNoFile = "Keine Datei ausgewählt / hochgeladen"; 
 $strSB_long = "Saison-Bestleistung";
-$strPB_long = "Persönliche Bestleistuung";
+$strPB_long = "Persönliche Bestleistung";
 
 ?>
