7c1ff3a5b2027e419dc750988a2adbb0111fbf53
rijoll@bluewin.ch <rijoll@bluewin.ch@77e7c866-7b45-0410-9efb-6b44a749e0e8>
2009-05-01 07:33:01 +0000

    Pendenz 614

---
+++ athletica/sql/upgrade_3_4_1.sql
@@ -0,0 +1,4 @@
+INSERT INTO `disziplin` (Kurzname,Name,Anzeige,Seriegroesse,Staffellaeufer,Typ,Appellzeit,Stellzeit,Strecke,Code, xOMEGA_Typ) VALUES 
+                       ('...KAMPF', '...kampf', 393, 6, 0, 9, '01:00:00','00:15:00', 4, 799, 1); 
+
+
