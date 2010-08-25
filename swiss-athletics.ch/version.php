7233f6dab207df8dd14b92f59461a2103f7f72ad
sbauer@mediasprint.ch <sbauer@mediasprint.ch@77e7c866-7b45-0410-9efb-6b44a749e0e8>
2010-08-25 13:16:01 +0000

    athletica versionsfile auf swiss-athletics.ch

---
+++ swiss-athletics.ch/version.php
@@ -0,0 +1,14 @@
+<?php
+/***********************
+ *
+ * Neueste Athletica-Version
+ *
+ *
+ **********************/
+
+$version = '4.0';
+$datum = '19.08.2010';
+
+echo "version=".$version."\n";
+echo "datum=".$datum."\n";
+?>
