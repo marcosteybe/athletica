fb15835f7439d1556aaae7fad2740bb465af5521
rijoll@bluewin.ch <rijoll@bluewin.ch@77e7c866-7b45-0410-9efb-6b44a749e0e8>
2011-07-18 06:22:16 +0000

    Fehler Einlesen Meeting: Grösse enthielt 408 wie Code

--- athletica/lib/cl_http_data.lib.php
+++ athletica/lib/cl_http_data.lib.php
@@ -140,9 +140,9 @@ document.getElementById("progress").width="150";
 		$totalbytes = 0;
 		while ($str = trim(fgets($sock, 4096))){
 			$headers .= "$str\n";
-			if(strpos($str, "400") !== false || strpos($str, "404") !== false || strpos($str, "408") !== false || strpos($str, "503") !== false){
+			if(strpos($str, " 400 ") !== false || strpos($str, " 404 ") !== false || strpos($str, " 408 ") !== false || strpos($str, " 503 ") !== false){
 				fclose($sock); // server return error (not found / bad request)
-				AA_printErrorMsg($strErrHttpBad);
+				AA_printErrorMsg($GLOBALS['strErrHttpBad']);
 				return false;
 			}
 		}     
